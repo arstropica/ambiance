d2c18cb2aa4e4da28ea4cfb8cadb40f54c9e72f7
ArsTropica <aowilliams@arstropica.com>
2015-02-05 17:14:12 -0500

    Initial Commit

---
+++ sidebar-secondary.php
@@ -0,0 +1,19 @@
+<?php
+
+/**
+ * Ambiance Theme sidebar-secondary.php
+ * 
+ * PHP version 5
+ * 
+ * @category   Theme Template File 
+ * @package    WordPress
+ * @author     ArsTropica <info@arstropica.com> 
+ * @copyright  2014 ArsTropica 
+ * @license    http://opensource.org/licenses/gpl-license.php GNU Public License 
+ * @version    1.0 
+ * @link       http://pear.php.net/package/ArsTropica  Reponsive Framework
+ * @subpackage Ambiance Theme
+ * @see        References to other sections (if any)...
+ */
+do_action('at_responsive_secondary_sidebar');
+?>
